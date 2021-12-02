1883259e54d00cd848b4467124ede1c5875bfa90
Arsen <62416244+arssssen@users.noreply.github.com>
2021-12-02 23:27:53 +0600

    practice3.2

---
+++ js/anim.js
@@ -0,0 +1,15 @@
+
+  anime({
+    targets: '.android',
+    translateX: -150,
+    loop:true,
+    direction:'alternate',
+    easing: 'easeInOutExpo'
+  });
+  anime({
+    targets: '.develop',
+    translateX: 150,
+    loop:true,
+    direction:'alternate',
+    easing: 'easeInOutExpo'
+  });
